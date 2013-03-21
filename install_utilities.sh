a368e41128f7d28775ed607f19de294e8cff9bfa
Haney Maxwell <hmrmaxwell@gmail.com>
2013-03-21 14:53:27 -0500

    adding chrome stuff

--- install_utilities.sh
+++ install_utilities.sh
@@ -2,3 +2,8 @@
 sudo apt-get install curl wget dpkg gnome-shell -yq
 cp new_dircolors ~/.dircolors
 gsettings set org.gnome.desktop.background picture-uri file://`pwd`/SimpleUbuntu.png
+
+#chrome install
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i ./google-chrome*.deb
+sudo apt-get -f install
