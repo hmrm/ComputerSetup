1b2b8658533e04111687415a0051ff513a596ab5
Haney Maxwell <hmrmaxwell@gmail.com>
2013-03-24 13:21:51 -0500

    ruby is so annoying

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 \curl -L https://get.rvm.io | bash -s stable --ruby
+rvm pkg install openssl
+rvm reinstall all --force
+gem install rails
--- install_utilities.sh
+++ install_utilities.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install curl wget dpkg gnome-shell ack-grep graphviz w3m build-essential gcc libc6-dev autoconf sqlite3 m4 libncurses-dev openssl flex bison xsltproc fop libwxgtk2.8-dev libgl1-mesa-dev libglu1-mesa-dev libpng3 aptitude g++ gfortran open-cobol pdflatex automake re2c libtool php5 r-base-dev htop gnugo -yq
+sudo apt-get install curl wget dpkg gnome-shell ack-grep graphviz w3m build-essential gcc libc6-dev autoconf sqlite3 m4 libncurses-dev openssl flex bison xsltproc fop libwxgtk2.8-dev libgl1-mesa-dev libglu1-mesa-dev libpng3 aptitude g++ gfortran open-cobol pdflatex automake re2c libtool php5 r-base-dev htop gnugo libyaml-0-2 -yq
 cp new_dircolors ~/.dircolors
 gsettings set org.gnome.desktop.background picture-uri file://`pwd`/SimpleUbuntu.png
 
