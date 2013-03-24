622be1deaee2a5980c352b52738a2fd974430823
Haney Maxwell <hmrmaxwell@gmail.com>
2013-03-24 16:35:17 -0700

    fixing annoying cabal 'feature'

--- install_haskell.sh
+++ install_haskell.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo apt-get install ghc ghc-prof ghc-doc cabal-install -yq
+sudo apt-get install ghc ghc-prof ghc-doc cabal-install haskell-doc -yq
 cabal update
+PATH=$PATH:~/.cabal/bin
+cabal install happy alex
 cabal install hlint ghc-mod
