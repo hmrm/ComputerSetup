73a824c59ad63fd359d9a2349d174d8b37160a75
Haney Maxwell <hmrmaxwell@gmail.com>
2013-03-25 09:07:48 -0700

    making python3 the latest

--- install_python.sh
+++ install_python.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt-get install python python3 python-pip -yq
+sudo apt-get install python python3.3 python-pip -yq
 sudo pip install django
 sudo apt-get install python-numpy python-scipy -yq
