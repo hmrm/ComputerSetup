03b007e9fbfd527c07acef660026f501bd37334f
Haney Maxwell <hmrmaxwell@gmail.com>
2013-03-24 23:56:52 -0700

    fixing prompt bug

--- bash_setup.sh
+++ bash_setup.sh
@@ -5,6 +5,7 @@ cp -v new_bash_aliases.sh ~/.bash_aliases;
 cp -v new_profile.sh ~/.profile;
 cp -v new_screenrc.sh ~/.screenrc
 wget https://raw.github.com/trapd00r/LS_COLORS/master/LS_COLORS -O $HOME/.dircolors;
+cp -r git-prompt ~
 cd git-prompt
 sudo make
 sudo make install
