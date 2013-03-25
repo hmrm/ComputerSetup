3bafe71f44a34319965eb6a38a74a3c625bcc175
Haney Maxwell <hmrmaxwell@gmail.com>
2013-03-25 00:05:04 -0700

    yet another attempt

--- bash_setup.sh
+++ bash_setup.sh
@@ -4,6 +4,7 @@ cp -v new_bashrc.sh ~/.bashrc;
 cp -v new_bash_aliases.sh ~/.bash_aliases;
 cp -v new_profile.sh ~/.profile;
 cp -v new_screenrc.sh ~/.screenrc
+cp -v new_xinitrc.sh ~/.xinitrc
 wget https://raw.github.com/trapd00r/LS_COLORS/master/LS_COLORS -O $HOME/.dircolors;
 cp -r git-prompt ~
 cd git-prompt
--- new_bashrc.sh
+++ new_bashrc.sh
@@ -5,9 +5,6 @@
 # If not running interactively, don't do anything
 [ -z "$PS1" ] && return
 
-# Start up emacs daemon
-emacs --daemon
-
 # don't put duplicate lines or lines starting with space in the history.
 # See bash(1) for more options
 HISTCONTROL=ignoreboth
