78310efef2a9312078165aa8fc95dd0769287b65
Haney Maxwell <hmrmaxwell@gmail.com>
2013-03-25 12:59:42 -0700

    no longer automatically tracking public keys

--- ssh_setup.sh
+++ ssh_setup.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 sudo apt-get install openssh-client openssh-server -yq;
 ssh-keygen -t rsa
-#TODO: setup a separate probably-gist for this
-cat ~/.ssh/id_rsa.pub >> publickeys
-git add publickeys
-git commit -m "added new key for new machine"
-git push origin master
