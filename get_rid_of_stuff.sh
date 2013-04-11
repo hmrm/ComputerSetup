1db570cac7bdbf9d02a93eace61d345cb4ea9a11
Haney Maxwell <hmrmaxwell@gmail.com>
2013-04-11 09:05:38 -0500

    -rf is a useful flag when you want to delete directories...

--- get_rid_of_stuff.sh
+++ get_rid_of_stuff.sh
@@ -2,5 +2,5 @@
 sudo rm -rf ~/.local/share/ubuntuone
 rm -rf ~/.cache/ubuntuone
 rm -rf ~/.config/ubuntuone
-rm ~/Ubuntu\ One
+rm -rf ~/Ubuntu\ One
 sudo apt-get purge ubuntuone-client rhythmbox aisleriot gnome-mahjongg gnome-sudoku gnomine unity thunderbird gwibber totem empathy remmina unity-lens-shopping unity-scope-video-remote unity-scope-musicstores shotwell -yq
