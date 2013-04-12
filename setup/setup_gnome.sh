6dc1526267391259887fdcf97e25ccf7c73ba3ce
Haney Maxwell <hmrmaxwell@gmail.com>
2013-04-12 18:02:08 -0500

    fixing the path of the image in gnome setup

--- setup/setup_gnome.sh
+++ setup/setup_gnome.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gsettings set org.gnome.desktop.background picture-uri file://`pwd`/SimpleUbuntu.png
+gsettings set org.gnome.desktop.background picture-uri file://`pwd`/media/SimpleUbuntu.png
 gconftool-2 --set /apps/gnome-terminal/global/use_menu_accelerators --type bool false
 gconftool-2 --set /apps/gnome-terminal/global/use_mnemonics --type bool false
 gconftool-2 --set /apps/gnome-terminal/keybindings/help --type string "disabled"
