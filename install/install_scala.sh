7ac8f995b8cce5c2bfa8c52f9360e8e27ca565be
Haney Maxwell <hmrmaxwell@gmail.com>
2013-04-12 18:19:22 -0500

    moveing scala emacs setup into emacs_setup

--- install/install_scala.sh
+++ install/install_scala.sh
@@ -8,10 +8,3 @@ mv ~/scala-2.10.1 ~/.scala
 wget http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt//0.12.2/sbt.deb
 sudo dpkg -i sbt.deb
 
-#installing ensime
-wget "https://dl.dropbox.com/sh/ryd981hq08swyqr/ZiCwjjr_vm/ENSIME%20Releases/ensime_2.10.0-0.9.8.9.tar.gz?token_hash=AAHpPjuDRijzZt1Qhvh6BJnRiaLjPCtPGMzvd-x5VIHcJA&dl=1" -O ensime.tar.gz
-tar xzvf ensime.tar.gz -C ~/.emacs.d
-mv ~/.emacs.d/ensime_2.10.0-0.9.8.9 ~/.emacs.d/ensime
-mkdir ~/.sbt
-mkdir ~/.sbt/plugins
-echo 'addSbtPlugin("org.ensime" % "ensime-sbt-cmd" % "VERSION")' >> ~/.sbt/plugins/plugins.sbt
--- setup/emacs_setup.sh
+++ setup/emacs_setup.sh
@@ -10,3 +10,11 @@ done
 
 # Assumes git is set up
 git clone https://github.com/hmrm/emacs-for-python.git ~/.emacs.d/emacs-for-python
+
+#installing ensime
+wget "https://dl.dropbox.com/sh/ryd981hq08swyqr/ZiCwjjr_vm/ENSIME%20Releases/ensime_2.10.0-0.9.8.9.tar.gz?token_hash=AAHpPjuDRijzZt1Qhvh6BJnRiaLjPCtPGMzvd-x5VIHcJA&dl=1" -O ensime.tar.gz
+tar xzvf ensime.tar.gz -C ~/.emacs.d
+mv ~/.emacs.d/ensime_2.10.0-0.9.8.9 ~/.emacs.d/ensime
+mkdir ~/.sbt
+mkdir ~/.sbt/plugins
+echo 'addSbtPlugin("org.ensime" % "ensime-sbt-cmd" % "VERSION")' >> ~/.sbt/plugins/plugins.sbt
