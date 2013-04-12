997a4abb32089605eb34ce047e0a63ee63dff81f
Stephen Rosen <sirosen@uchicago.edu>
2013-04-12 16:03:44 -0500

    Began separating install and setup.

---
+++ install/versioncontrol_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+apt-get install git -yq;
+apt-get install subversion -yq;
+apt-get install mercurial -yq;
--- setup/versioncontrol_setup.sh
+++ setup/versioncontrol_setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo apt-get install git -yq;
 echo "Enter your name:"
 read name
 git config --global user.name $name
@@ -7,4 +6,3 @@ echo "Enter your email:"
 read email
 git config --global user.email $email
 git config --global color.ui true
-sudo apt-get install mercurial subversion -yq;
