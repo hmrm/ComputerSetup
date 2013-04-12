3a46206e0a586049f49fc441a864cbc39a979aec
Haney Maxwell <hmrmaxwell@gmail.com>
2013-04-12 18:08:01 -0500

    terser install_clojure.sh

--- install/install_clojure.sh
+++ install/install_clojure.sh
@@ -2,7 +2,7 @@
 #install leiningen
 wget https://raw.github.com/technomancy/leiningen/stable/bin/lein
 mkdir ~/bin
-mv lein ~/bin/lein
+mv lein ~/bin
 chmod a+x ~/bin/lein
 PATH=$PATH:~/bin
 lein repl :headless &
