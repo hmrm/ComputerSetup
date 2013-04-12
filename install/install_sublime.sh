ec72b652e797e406cfe4cbc2409434e70a80d7f9
Haney Maxwell <hmrmaxwell@gmail.com>
2013-04-12 18:48:44 -0500

    decomposition of install_utilities

---
+++ install/install_sublime.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#sublime text install
+sudo add-apt-repository ppa:webupd8team/sublime-text-2 -y
+sudo apt-get update
+sudo apt-get install sublime-text -yq
--- install/install_utilities.sh
+++ install/install_utilities.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install mutt mailutils curl wget dpkg gnome-shell ack-grep graphviz w3m build-essential gcc libc6-dev autoconf sqlite3 m4 libncurses-dev openssl flex bison xsltproc fop libwxgtk2.8-dev libgl1-mesa-dev libglu1-mesa-dev libpng3 aptitude g++ gfortran open-cobol texlive automake re2c libtool php5 r-base-dev htop gnugo libyaml-0-2 git-core libyaml-dev libsqlite3-dev libxml2-dev libxslt1-dev libgdbm-dev -yq
-
-#chrome install
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i ./google-chrome*.deb
-sudo apt-get -f install
-
-#sublime text install
-sudo add-apt-repository ppa:webupd8team/sublime-text-2 -y
-sudo apt-get update
-sudo apt-get install sublime-text -yq
