8ab664e0f9dc1b3c25b3ab1c6c1ee24d8361d85b
Haney Maxwell <hmrmaxwell@gmail.com>
2013-05-16 12:17:54 -0500

    smaller font for more stuff on screen

--- setup/gnome.sh
+++ setup/gnome.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/font --type string "monospace 10"
+gconftool-2 --set /apps/gnome-terminal/profiles/Default/use_system_font --type bool false
 gconftool-2 --set /apps/gnome-terminal/global/use_menu_accelerators --type bool false
 gconftool-2 --set /apps/gnome-terminal/global/use_mnemonics --type bool false
 gconftool-2 --set /apps/gnome-terminal/keybindings/help --type string "disabled"
-
