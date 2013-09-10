4bce24d1d080d0b0da008977af33cc19a6fb6312
Haney Maxwell <hmrmaxwell@gmail.com>
2013-09-09 23:36:19 -0700

    Added alarm-clock to utilityies

--- install/utilities.sh
+++ install/utilities.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 apt-get update
-apt-get install mutt mailutils curl wget dpkg gnome-shell ack-grep graphviz w3m build-essential gcc libc6-dev autoconf sqlite3 m4 libncurses-dev openssl flex bison xsltproc fop libwxgtk2.8-dev libgl1-mesa-dev libglu1-mesa-dev libpng3 aptitude clang g++ gfortran open-cobol texlive automake re2c libtool php5 r-base-dev htop gnugo libyaml-0-2 git-core libyaml-dev libsqlite3-dev libxml2-dev libxslt1-dev libgdbm-dev iptraf markdown pandoc valgrind nmap -yq
+apt-get install mutt mailutils curl wget dpkg gnome-shell ack-grep graphviz w3m build-essential gcc libc6-dev autoconf sqlite3 m4 libncurses-dev openssl flex bison xsltproc fop libwxgtk2.8-dev libgl1-mesa-dev libglu1-mesa-dev libpng3 aptitude clang g++ gfortran open-cobol texlive automake re2c libtool php5 r-base-dev htop gnugo libyaml-0-2 git-core libyaml-dev libsqlite3-dev libxml2-dev libxslt1-dev libgdbm-dev iptraf markdown pandoc valgrind nmap alarm-clock -yq
